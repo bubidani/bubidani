c58a2b122dd2e34939f4fbb14083560ca82ccb76
Sunmi <github@danbenfield.co.uk>
2023-05-26 13:24:15 +0100

    day 1 complete

--- PythonIn30/Day 1/day1.py
+++ PythonIn30/Day 1/day1.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
-var = 2 * 4
-print(var)
+
+xpoint1 = 2
+ypoint1 = 3
+xpoint2 = 10
+ypoint2 = 8
+
+length = xpoint2 - xpoint1
+height = ypoint2 - ypoint1
+
+pythag = (length ** 2 + height ** 2) ** 0.5
+
+print(pythag)
